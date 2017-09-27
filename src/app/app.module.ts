a2b3ec76107ff94877640d0b473cea4a79be57b5
wedaft <Guest@Epicodus-7D.local>
2017-09-27 11:49:55 -0700

    start of project

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { routing } from './app.routing';
+import { ProjectComponent } from './project/project.component';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    ProjectComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProjectComponent } from './project/project.component';
+import { AdminComponent } from './admin/admin.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'project',
+    component: ProjectComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
